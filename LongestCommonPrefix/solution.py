94f3aecfe91298180c201f356cd7dd2275e3a6d2
Hamza-Sarwar <hamzasarwar129@gmail.com>
2024-03-01 21:48:58 +0500

    LongestCommonPrefix solution rectified

--- LongestCommonPrefix/solution.py
+++ LongestCommonPrefix/solution.py
@@ -1,8 +1,10 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        res = ''
-        all_words = list(zip(strs[0], strs[1], strs[2]))
-        for x in all_words:
-            if x[0] == x[1] == x[2]:
-                res = res + str(x[0])
-        return res
+    def longestCommonPrefix(self, strs) -> str:
+        if not strs:
+            return ""
+        shortest_str = min(strs, key=len)
+        for i, char in enumerate(shortest_str):
+            for other_str in strs:
+                if other_str[i] != char:
+                    return shortest_str[:i]
+        return shortest_str
